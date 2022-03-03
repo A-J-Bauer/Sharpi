9ca22ec641305814ef18442243ad35bca3c5afdf
A-J-Bauer <a.j.bauer@hotmail.com>
2022-03-03 16:06:54 +0100

    new package test

--- .github/CheckVersionPublished/NugetVersions.cs
+++ .github/CheckVersionPublished/NugetVersions.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace CheckVersionPublished
 {
+    /// <summary>
+    /// Helper class for parsing json returned from
+    /// https://api.nuget.org/v3-flatcontainer/Sharpi/index.json
+    /// </summary>
     public class NugetVersions
     {        
         public string[]? versions { get; set; }
